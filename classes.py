a3fc79701f2d47b5cffdf60d8fcefebe855e9bbb
Allan Rios <rios.allan96@gmail.com>
2021-05-12 12:32:51 -0700

    Added the Drug class and several methods to create the drugs that are associated with each imported excel sheet. I added an additional col_scan method ("col_scan_value"), and updated the original to be "col_scan_bool". This way one could give me the boolean value and the other the actual value. Within ExcelWkbk class, I added to the get_worksheets function and by using my new "get_and_set_drugs" func. This function is repsponsible for scanning through to find the names, dilution factors, and concentration of the drugs. The clean_cell_name outer function strips the cell to its actual cell number ('A1'). That is then incremented by the inc_by_column function (ie. A1 -> B1) to get the concentration and incremented another to get the dilution factor. The way the imported excel sheet is set up makes this always true... The cell containing the drug name is in the A column, conc in B column, and dilution in C column. I will now begin to work on the barcodes.

--- classes.py
+++ classes.py
@@ -1,7 +1,7 @@
 import openpyxl
 
 
-def col_scan(wkst, desired_value, col_num):
+def col_scan_bool(wkst, desired_value, col_num):
     for col in wkst.iter_cols(col_num):
         for cell in col:
             if cell.value is not None and desired_value in str(cell.value):
@@ -9,31 +9,46 @@ def col_scan(wkst, desired_value, col_num):
     return False
 
 
-def string_extract(cell, string):
+def col_scan_value(wkst, desired_value, col_num):
+    for col in wkst.iter_cols(col_num):
+        for cell in col:
+            if cell.value is not None and desired_value in str(cell.value):
+                return cell
+
+
+def inc_by_column(cell_name, quantity):
+    return chr(ord(cell_name[0]) + quantity) + cell_name[1:]
+
+
+def string_extract(cell, split_char):
     cell_string = cell.value
-    if string in cell_string:
-        return_val = cell_string.split(string)
+    if split_char in cell_string:
+        return_val = cell_string.split(split_char)
         return "".join(return_val).strip()
     else:
-        print(string + " not found in cell A1\n")
+        print(split_char + " not found in cell A1\n")
+
+
+def clean_cell_name(cell):
+    return str(cell).split('.')[1].split('>')[0]
 
 
 def drug_check(wkst, col_number):
     for j in reversed(range(4)):
-        if col_scan(wkst, "Drug {}".format(str(j + 1)), col_number):
+        if col_scan_bool(wkst, "Drug {}".format(str(j + 1)), col_number):
             return j + 1
 
 
 class ExcelWkbk:
     def __init__(self, wkbk):
-        self.wkbk = wkbk
+        self.imported_wkbk = wkbk
         self.sheet_names = wkbk.sheetnames
         self.sheets = []
         self.technician = ""
 
     def get_worksheets(self):
         for sheet in range(len(self.sheet_names)):
-            ws = self.wkbk[self.sheet_names[sheet]]
+            ws = self.imported_wkbk[self.sheet_names[sheet]]
             self.technician = string_extract(ws['A1'], 'Name:')
             self.sheets.append(
                 ExcelSheet(
@@ -41,23 +56,53 @@ class ExcelWkbk:
                     num_of_drugs=drug_check(ws, 1)
                 )
             )
+            num_drugs = self.sheets[sheet].num_of_drugs
+            self.get_and_set_drugs(num_drugs, ws, sheet)
+
+    def get_and_set_drugs(self, num_drugs, wkst, sheet_index):
+        for i in range(num_drugs):
+            if col_scan_bool(wkst, 'Drug {}'.format(i + 1), 1):
+                cell_val = col_scan_value(wkst, 'Drug {}'.format(i + 1), 1)
+                drug_name = cell_val.value
+                cell_val = clean_cell_name(cell_val)
+                conc_cell = inc_by_column(cell_val, 1)
+                dilut_cell = inc_by_column(cell_val, 2)
+                for col in wkst.iter_cols(2, 3):
+                    for cell in col:
+                        if conc_cell in str(cell):
+                            conc_cell = cell.value
+                            break
+                        elif dilut_cell in str(cell):
+                            dilut_cell = cell.value
+                            break
+                self.sheets[sheet_index].append_to_drug_list(Drug(drug_name, conc_cell, dilut_cell))
 
     def get_num_of_sheets(self):
         return len(self.sheets)
 
     def print_sheet_info(self):
         print(str(self.get_num_of_sheets()) + " sheets in " + self.technician + "'s workbook")
+        print('*' * 35)
         for i in range(len(self.sheets)):
             self.sheets[i].display_info()
 
-# need to add more attributes including barcodes (control and treatment), drug names, etc.
+
 class ExcelSheet:
     def __init__(self, date, num_of_drugs):
         self.date = date
         self.num_of_drugs = num_of_drugs
+        self.drug_list = []
 
     def display_info(self):
-        print(str(self.num_of_drugs) + " drugs used on " + self.date + "\n")
+        print(str(self.num_of_drugs) + " drugs used on " + self.date)
+        for i in range(self.num_of_drugs):
+            drug_info = self.drug_list[i]
+            print(drug_info.name + "\n\tConcentration:" +
+                  str(drug_info.concentration) + "\n\tDilution: " + str(drug_info.dilution))
+        print('-' * 35)
+
+    def append_to_drug_list(self, drug):
+        self.drug_list.append(drug)
 
 
 class ControlPlate:
@@ -70,3 +115,10 @@ class ControlPlate:
             self.num_of_cell_lines = num_of_cell_lines
             self.cell_line_names = cell_line_names
             print("Success!\n")
+
+
+class Drug:
+    def __init__(self, name, concentration, dilution):
+        self.name = name
+        self.concentration = concentration
+        self.dilution = dilution
