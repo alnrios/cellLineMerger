c6535ad2cb5957324d8f8ec73d587907870b47a1
Allan Rios <rios.allan96@gmail.com>
2021-05-18 13:58:26 -0700

    working on the get_treatments function. added in a return list option for the cell_scan function. trying to figure out how to iterate through the spreadsheet and get the cell lines that i want

--- classes.py
+++ classes.py
@@ -2,6 +2,7 @@ import openpyxl
 
 
 def cell_scan(wkst, desired_value, col_num, type_of_scan):
+    return_list = []  # used in the case that this function needs to return a list
     for col in wkst.iter_cols(col_num):
         for cell in col:
             if cell.value is not None:
@@ -17,8 +18,13 @@ def cell_scan(wkst, desired_value, col_num, type_of_scan):
                 elif type_of_scan == 'cell return bool':
                     if desired_value in str(cell):
                         return True
+                elif type_of_scan == 'value return list':   # returns list of cells that the value was found at
+                    if desired_value in str(cell.value):
+                        return_list.append(cell)
                 else:
                     print("Incorrect type of scan. Exit and try again...")
+    if type_of_scan == 'value return list':
+        return return_list
 
 
 def control_scan(wkst, desired_value, col_num):
@@ -38,6 +44,10 @@ def control_scan(wkst, desired_value, col_num):
 
 def inc_by_column(cell_name, quantity):
     return chr(ord(cell_name[0]) + quantity) + cell_name[1:]
+
+
+def inc_by_row(cell_name, quantity):
+    return cell_name[0] + str(int(cell_name[1:]) + quantity)
 
 
 # def insert_and_pop(my_list, )
@@ -61,6 +71,21 @@ def drug_check(wkst, col_number):
             return j + 1
 
 
+def get_cell_lines(wkst, barcode_cell, count):
+    cell_line_list = []
+    pos = 0     # records the position of the cell line
+    while True:
+        pos += 1
+        barcode_cell = inc_by_column(barcode_cell, 1)
+        count += 1
+        cell_line = cell_scan(wkst, barcode_cell, count, 'cell return cell')
+        if cell_line is not None:
+            cell_line_list.append(CellLine(cell_line.value, pos))
+        else:
+            break
+    return cell_line_list
+
+
 class ExcelWkbk:
     def __init__(self, wkbk):
         self.imported_wkbk = wkbk
@@ -81,6 +106,7 @@ class ExcelWkbk:
             num_drugs = self.sheets[sheet].num_of_drugs
             self.get_and_set_drugs(num_drugs, ws, sheet)
             self.get_controls(ws, sheet)
+            self.get_treatments(ws, sheet)
 
     def get_and_set_drugs(self, num_drugs, wkst, sheet_index):
         for i in range(num_drugs):
@@ -103,12 +129,6 @@ class ExcelWkbk:
     def get_num_of_sheets(self):
         return len(self.sheets)
 
-    def display_sheet_info(self):
-        print(str(self.get_num_of_sheets()) + " sheets in " + self.technician + "'s workbook")
-        print('*' * 35)
-        for i in range(len(self.sheets)):
-            self.sheets[i].display_info()
-
     def get_controls(self, wkst, sheet_index):
         control_xl_dict = {
             'd1_xl_cells': control_scan(wkst, ['Day1', 'Day 1'], 1),
@@ -118,7 +138,7 @@ class ExcelWkbk:
             for xl_iter in range(len(control_xl_dict[i])):  # iterator for the control_xl_dict items in lists
                 barcode = inc_by_column(control_xl_dict[i][xl_iter], 1)   # first assigns barcode var to the cell number
                 col_count = 2  # col_count refers to the column number that'll be used, which will always be 2 for bc's
-                cell_line_list = self.get_cell_lines(wkst, barcode, col_count)
+                cell_line_list = get_cell_lines(wkst, barcode, col_count)
                 barcode = cell_scan(wkst, barcode, col_count, 'cell return cell').value
                 if i == 'd1_xl_cells':
                     self.sheets[sheet_index].day1_list.append(
@@ -127,40 +147,46 @@ class ExcelWkbk:
                     self.sheets[sheet_index].day1_list.append(
                         ControlPlate(barcode, len(cell_line_list), cell_line_list, 'd7'))
 
-#######################################################################################################################
+    def display_sheet_info(self):
+        print(str(self.get_num_of_sheets()) + " sheets in " + self.technician + "'s workbook")
+        print('*' * 35)
+        for i in range(len(self.sheets)):
+            self.sheets[i].display_info()
 
-    def get_cell_lines(self, wkst, barcode_cell, count):
-        cell_line_list = []
-        pos = 0     # records the position of the cell line
-        while True:
-            pos += 1
-            barcode_cell = inc_by_column(barcode_cell, 1)
-            count += 1
-            cell_line = cell_scan(wkst, barcode_cell, count, 'cell return cell')
-            if cell_line is not None:
-                cell_line_list.append(CellLine(cell_line.value, pos))
-            else:
-                break
-        return cell_line_list
-
+    def get_treatments(self, wkst, sheet_index):
+        if cell_scan(wkst, 'TREATMENT PLATES', 1, 'value return bool'):
+            # below is a list comprised of cells that contain "TREATMENT PLATES as its value"
+            treat_cells = cell_scan(wkst, 'TREATMENT PLATES', 1, 'value return list')
+            for i in range(len(treat_cells)):
+                for j in range(self.sheets[sheet_index].num_of_drugs):
+                    cleaned_cell = clean_cell_name(treat_cells[i])
+                    cleaned_cell = inc_by_row(cleaned_cell, j + 1)
+                    drug = wkst[cleaned_cell].value
+                    cleaned_cell = inc_by_column(cleaned_cell, 1)
+                    barcode = wkst[cleaned_cell].value
+                    # if barcode is not None and 'Treatment' not in barcode:
+                    # in the middle of creating this function. I am trying to iterate to capture
+                    # the cell lines
 
 
 
 
 
 class ExcelSheet:
+    drug_list = []
+    day1_list = []
+    day7_list = []
+    treatment_list = []
+
     def __init__(self, date, num_of_drugs):
         self.date = date
         self.num_of_drugs = num_of_drugs
-        self.drug_list = []
-        self.day1_list = []
-        self.day7_list = []
 
     def display_info(self):
         print(str(self.num_of_drugs) + " drugs used on " + self.date)
         for i in range(self.num_of_drugs):
             drug_info = self.drug_list[i]
-            print(drug_info.name + "\n\tConcentration:" +
+            print(drug_info.name + "\n\tConcentration: " +
                   str(drug_info.concentration) + "\n\tDilution: " + str(drug_info.dilution))
         print('-' * 35)
         print("Control Plates:")
@@ -191,7 +217,7 @@ class ControlPlate(Plate):
         self.control_type = control_type
 
     def __str__(self):
-        return str(self.num_of_cell_lines) + " cell lines in " + self.control_type + " plate " + \
+        return str(self.num_of_cell_lines) + " cell lines in " + self.control_type + " plate - " + \
                self.barcode + ": " + self.display_cell_lines()
 
     def display_cell_lines(self):
