52129c4d149577f1b0100d9b58ed5ff8a610bb36
Allan Rios <rios.allan96@gmail.com>
2021-05-19 15:22:23 -0700

    The get_treatments function has been completed. The get_worksheets function has been updated to call the get_treatments one and give each worksheet a list of treatment plates that contain the cells, the drug it is treated with, and its day1 and day 7 links. The day 1 and day 7 links are necessary to be able to keep track of where the controls reside. The get_treatments function incorporates the get_control_links function which basically just searches for a control plate and makes sure each cell line from the current treatment plate is also located on a control plate. This is done for both day 1 and day 7 plates.

--- classes.py
+++ classes.py
@@ -18,11 +18,11 @@ def cell_scan(wkst, desired_value, col_num, type_of_scan):
                 elif type_of_scan == 'cell return bool':
                     if desired_value in str(cell):
                         return True
-                elif type_of_scan == 'value return list':   # returns list of cells that the value was found at
+                elif type_of_scan == 'value return list':  # returns list of cells that the value was found at
                     if desired_value in str(cell.value):
                         return_list.append(cell)
                 else:
-                    print("Incorrect type of scan. Exit and try again...")
+                    raise ValueError("Incorrect type of scan. Exit and try again...")
     if type_of_scan == 'value return list':
         return return_list
 
@@ -71,9 +71,9 @@ def drug_check(wkst, col_number):
             return j + 1
 
 
-def get_cell_lines(wkst, barcode_cell, count):
+def get_control_cell_lines(wkst, barcode_cell, count):
     cell_line_list = []
-    pos = 0     # records the position of the cell line
+    pos = 0  # records the position of the cell line
     while True:
         pos += 1
         barcode_cell = inc_by_column(barcode_cell, 1)
@@ -94,9 +94,10 @@ class ExcelWkbk:
         self.technician = ""
 
     def get_worksheets(self):
+        ws = self.imported_wkbk[self.sheet_names[0]]
+        self.technician = string_extract(ws['A1'], 'Name:')
         for sheet in range(len(self.sheet_names)):
             ws = self.imported_wkbk[self.sheet_names[sheet]]
-            self.technician = string_extract(ws['A1'], 'Name:')
             self.sheets.append(
                 ExcelSheet(
                     date=string_extract(ws['A2'], 'SET UP DATE:'),
@@ -136,15 +137,15 @@ class ExcelWkbk:
         }
         for i in control_xl_dict:
             for xl_iter in range(len(control_xl_dict[i])):  # iterator for the control_xl_dict items in lists
-                barcode = inc_by_column(control_xl_dict[i][xl_iter], 1)   # first assigns barcode var to the cell number
+                barcode = inc_by_column(control_xl_dict[i][xl_iter], 1)  # first assigns barcode var to the cell number
                 col_count = 2  # col_count refers to the column number that'll be used, which will always be 2 for bc's
-                cell_line_list = get_cell_lines(wkst, barcode, col_count)
+                cell_line_list = get_control_cell_lines(wkst, barcode, col_count)
                 barcode = cell_scan(wkst, barcode, col_count, 'cell return cell').value
                 if i == 'd1_xl_cells':
                     self.sheets[sheet_index].day1_list.append(
                         ControlPlate(barcode, len(cell_line_list), cell_line_list, 'd1'))
-                else:
-                    self.sheets[sheet_index].day1_list.append(
+                elif i == 'd7_xl_cells':
+                    self.sheets[sheet_index].day7_list.append(
                         ControlPlate(barcode, len(cell_line_list), cell_line_list, 'd7'))
 
     def display_sheet_info(self):
@@ -158,29 +159,47 @@ class ExcelWkbk:
             # below is a list comprised of cells that contain "TREATMENT PLATES as its value"
             treat_cells = cell_scan(wkst, 'TREATMENT PLATES', 1, 'value return list')
             for i in range(len(treat_cells)):
+                count = 0  # this count var is necessary to know how many cells to go backwards when capturing
+                # cell line positions (ie. Column 1-2, Column 3-4)
                 for j in range(self.sheets[sheet_index].num_of_drugs):
                     cleaned_cell = clean_cell_name(treat_cells[i])
                     cleaned_cell = inc_by_row(cleaned_cell, j + 1)
                     drug = wkst[cleaned_cell].value
                     cleaned_cell = inc_by_column(cleaned_cell, 1)
-                    barcode = wkst[cleaned_cell].value
-                    # if barcode is not None and 'Treatment' not in barcode:
-                    # in the middle of creating this function. I am trying to iterate to capture
-                    # the cell lines
-
-
-
+                    if wkst[cleaned_cell].value is not None and 'Treatment' not in wkst[cleaned_cell].value:
+                        barcode = wkst[cleaned_cell].value
+                        cell_lines = []
+                        while True:
+                            cleaned_cell = inc_by_column(cleaned_cell, 1)
+                            position_cell = inc_by_row(cleaned_cell, count - 1)
+                            if wkst[cleaned_cell].value is None or 'Cell Line' in wkst[cleaned_cell].value:
+                                break
+                            else:
+                                cell_lines.append(CellLine(wkst[cleaned_cell].value, wkst[position_cell].value))
+                        for listed_drug in self.sheets[sheet_index].drug_list:
+                            if drug == listed_drug.name:
+                                drug = listed_drug
+                        import_treatment = TreatmentPlate(barcode, len(cell_lines), cell_lines, drug)
+                        import_treatment.get_control_links('d1', self.sheets[sheet_index])
+                        import_treatment.get_control_links('d7', self.sheets[sheet_index])
+                        self.sheets[sheet_index].treatment_list.append(import_treatment)
+                    count -= 1
+                    '''
+                    ^^^^^^^^
+                    The count variable is subtracted above here this far back in the loop because the positions will get
+                    further from the actual cell line that is being processed. Simply subtracting 1 every time will not
+                    cut it, especially when trying to account for multiple drugs that could reach a count of 5.
+                    '''
 
 
 class ExcelSheet:
-    drug_list = []
-    day1_list = []
-    day7_list = []
-    treatment_list = []
-
     def __init__(self, date, num_of_drugs):
         self.date = date
         self.num_of_drugs = num_of_drugs
+        self.drug_list = []
+        self.day1_list = []
+        self.day7_list = []
+        self.treatment_list = []
 
     def display_info(self):
         print(str(self.num_of_drugs) + " drugs used on " + self.date)
@@ -192,13 +211,14 @@ class ExcelSheet:
         print("Control Plates:")
         for i in range(len(self.day1_list)):
             print("\t" + str(self.day1_list[i]))
+        for j in range(len(self.day7_list)):
+            print("\t" + str(self.day7_list[j]))
+        print("Treatment Plates")
+        for k in range(len(self.treatment_list)):
+            print("\t" + str(self.treatment_list[k]))
 
     def append_to_drug_list(self, drug):
         self.drug_list.append(drug)
-
-    # def append_to_control_list(self, control_type):
-    #     if control_type == 'd1':
-
 
 
 class Plate:
@@ -206,19 +226,6 @@ class Plate:
         self.barcode = barcode
         self.num_of_cell_lines = num_of_cell_lines
         self.cell_line_list = cell_line_list
-
-    # def append_cell_lines(self, cell_line):
-    #     self.cell_line_list.append(cell_line)
-
-
-class ControlPlate(Plate):
-    def __init__(self, barcode, num_of_cell_lines, cell_line_list, control_type):
-        super().__init__(barcode, num_of_cell_lines, cell_line_list)
-        self.control_type = control_type
-
-    def __str__(self):
-        return str(self.num_of_cell_lines) + " cell lines in " + self.control_type + " plate - " + \
-               self.barcode + ": " + self.display_cell_lines()
 
     def display_cell_lines(self):
         return_string = "["
@@ -231,11 +238,58 @@ class ControlPlate(Plate):
         return return_string
 
 
+class ControlPlate(Plate):
+    def __init__(self, barcode, num_of_cell_lines, cell_line_list, control_type):
+        super().__init__(barcode, num_of_cell_lines, cell_line_list)
+        self.control_type = control_type
+
+    def __str__(self):
+        return str(self.num_of_cell_lines) + " cell lines in " + self.control_type + " plate - " + \
+               self.barcode + ": " + self.display_cell_lines()
+
+    # def display_cell_lines(self):
+    #     return_string = "["
+    #     for i in range(self.num_of_cell_lines):
+    #         if i == self.num_of_cell_lines - 1:
+    #             return_string += (self.cell_line_list[i].name + ": " + str(self.cell_line_list[i].position))
+    #         else:
+    #             return_string += (self.cell_line_list[i].name + ": " + str(self.cell_line_list[i].position) + ", ")
+    #     return_string += "]"
+    #     return return_string
+
+
 class TreatmentPlate(Plate):
-    def __init__(self):
-        super().__init__("TreatmentPlate")
+    def __init__(self, barcode, num_of_cell_lines, cell_line_list, drug):
+        super().__init__(barcode, num_of_cell_lines, cell_line_list)
+        self.drug = drug
         self.day1_link = ""
         self.day7_link = ""
+
+    def get_control_links(self, control_type, wkst):
+        if control_type == 'd1':
+            control_list = wkst.day1_list
+        elif control_type == 'd7':
+            control_list = wkst.day7_list
+        else:
+            raise ValueError("Incorrect control type inputted into the 'get_control_links' function")
+        for control_plate in control_list:
+            pass_var = True
+            ctrl_cell_line_list = []
+            for i in range(len(control_plate.cell_line_list)):
+                ctrl_cell_line_list.append(control_plate.cell_line_list[i].name)
+            for cell_line in self.cell_line_list:
+                if cell_line.name not in ctrl_cell_line_list:
+                    pass_var = False
+                    break
+            if pass_var and control_type == 'd1':
+                self.day1_link = control_plate
+            elif pass_var and control_type == 'd7':
+                self.day7_link = control_plate
+            else:
+                pass
+
+    def __str__(self):
+        return self.barcode + ": " + self.display_cell_lines() + " treated with " + str(self.drug)
 
 
 class Drug:
@@ -243,6 +297,9 @@ class Drug:
         self.name = name
         self.concentration = concentration
         self.dilution = dilution
+
+    def __str__(self):
+        return self.name
 
 
 class CellLine:
